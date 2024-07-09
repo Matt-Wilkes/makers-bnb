e9d23f0cd29dd094c5e820d20e3c435f44bbe1ee
Matt Wilkes <matthew.wilkes90@gmail.com>
2024-07-09 15:28:07 +0100

    add more details to the view_space page

--- tests/test_app.py
+++ tests/test_app.py
@@ -22,4 +22,18 @@ def test_get_view_space(page, test_web_address, db_connection):
     db_connection.seed('seeds/space.sql')
     page.goto(f"http://{test_web_address}/view_space/1")
     h1_tag = page.locator("h1")
-    expect(h1_tag).to_have_text("A lovely place to stay")
+    expect(h1_tag).to_have_text("Makers Mansion")
+
+"""
+The view_space page should show the property details (name, description, bedrooms, price (GBP), location)
+"""
+
+def test_get_view_space_properties(page, test_web_address, db_connection):
+    db_connection.seed('seeds/space.sql')
+    page.goto(f"http://{test_web_address}/view_space/1")
+    expect(page.get_by_test_id("t-description")).to_have_text("A lovely place to stay")
+    expect(page.get_by_test_id("t-bedrooms")).to_have_text("3 Bedrooms")
+    expect(page.get_by_test_id("t-price")).to_have_text("£100")
+    expect(page.get_by_test_id("t-location")).to_have_text("London, UK")
+    
+    
