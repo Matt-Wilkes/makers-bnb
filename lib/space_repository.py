e3a4690913ab77da6b15f69bd887453e096682fa
Matt Wilkes <matthew.wilkes90@gmail.com>
2024-07-10 12:31:34 +0100

    fixed failing tests on space and space_repository, added owner to space class

--- lib/space_repository.py
+++ lib/space_repository.py
@@ -7,14 +7,14 @@ class SpaceRepository:
     def get_all(self):
         rows = self.db_connection.execute("SELECT * FROM spaces")
         print(rows)
-        return [Space(row['id'], row['description'], row['name'], row['bedrooms'], row['price'], row['country'], row['city'], row['booked_dates']) for row in rows]
+        return [Space(row['id'], row['description'], row['name'], row['bedrooms'], row['price'], row['country'], row['city'], row['booked_dates'],row['owner_id']) for row in rows]
     
     def get_by_id(self,id):
         rows = self.db_connection.execute("SELECT * FROM spaces WHERE id = %s",[id])
-        return Space(rows[0]['id'], rows[0]['description'], rows[0]['name'], rows[0]['bedrooms'], rows[0]['price'], rows[0]['country'], rows[0]['city'], rows[0]['booked_dates'])
+        return Space(rows[0]['id'], rows[0]['description'], rows[0]['name'], rows[0]['bedrooms'], rows[0]['price'], rows[0]['country'], rows[0]['city'], rows[0]['booked_dates'], rows[0]['owner_id'])
     
     def create(self, space):
-        self.db_connection.execute("INSERT INTO spaces (description, name, bedrooms, price, country, city, booked_dates) VALUES (%s, %s, %s, %s, %s, %s, %s)", [space.description, space.name, space.bedrooms, space.price, space.country, space.city, space.booked_dates])
+        self.db_connection.execute("INSERT INTO spaces (description, name, bedrooms, price, country, city, booked_dates, owner_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", [space.description, space.name, space.bedrooms, space.price, space.country, space.city, space.booked_dates, space.owner_id])
     
     def delete(self, id):
         self.db_connection.execute("DELETE FROM spaces WHERE id = %s", [id])
--- tests/test_space.py
+++ tests/test_space.py
@@ -12,10 +12,11 @@ def test_init(created_space):
     assert created_space.country == "UK"
     assert created_space.city == "London"
     assert created_space.booked_dates == []
+    assert created_space.owner_id == "email.1@gmail.com"
 
 def test_is_equal(created_space):
-    space1 = Space(1, "A space for testing", "Test Space", 2, 100, "UK", "London", [])
+    space1 = Space(1, "A space for testing", "Test Space", 2, 100, "UK", "London", [], 'email.1@gmail.com')
     assert created_space == space1
 
 def test_string_format(created_space):
-    assert str(created_space) == "Space(1, A space for testing, Test Space, 2, 100, UK, London, [])"
+    assert str(created_space) == "Space(1, A space for testing, Test Space, 2, 100, UK, London, [], email.1@gmail.com)"
