6bc72cfe0c4bb5698770ab9d51c48479f0c2c729
Matt Wilkes <matthew.wilkes90@gmail.com>
2024-07-11 17:06:54 +0100

    booking form only shows available dates in the future

--- view_space_routes.py
+++ view_space_routes.py
@@ -3,8 +3,6 @@ from lib.space_repository import SpaceRepository
 from lib.bookings import Bookings
 from lib.bookings_repository import BookingsRepository
 from flask import request, render_template, redirect, url_for
-
-import calendar
 from datetime import datetime
 
 
@@ -16,16 +14,11 @@ def apply_space_routes(app):
         space_repository = SpaceRepository(connection)
         bookings_repository = BookingsRepository(connection)
         space = space_repository.get_by_id(id)
-        cal= calendar.Calendar()
         available_bookings = bookings_repository.get_by_spaces_id_available(id)
         available_dates = []
         for booking in available_bookings:
-            available_dates.append(booking.date)
-        # for date in month_dates:
-        #     # date = datetime.date 
-        #     # datetime.today() datetime.datetime
-        #     if date > datetime.today().date() and date not in space.booked_dates:
-        #         available_dates.append(date)
+            if booking.date > datetime.today().date():
+                available_dates.append(booking)
             
         return render_template('view-space.html', space=space, available_dates=available_dates)
     
