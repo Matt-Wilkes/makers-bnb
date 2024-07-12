3bfea682af15f7bf5f553c011af6edf3126dcd68
Aivaras Voleika <154103843+aivocode@users.noreply.github.com>
2024-07-12 10:31:17 +0100

    Create new space

--- lib/space_repository.py
+++ lib/space_repository.py
@@ -3,21 +3,23 @@ from lib.space import Space
 class SpaceRepository:
     def __init__(self, db_connection):
         self.db_connection = db_connection
-        
+
     def get_all(self):
         rows = self.db_connection.execute("SELECT * FROM spaces")
         print(rows)
         return [Space(row['id'], row['description'], row['name'], row['bedrooms'], row['price'], row['country'], row['city'], row['booked_dates'],row['owner_id']) for row in rows]
-    
+
     def get_by_id(self,id):
         rows = self.db_connection.execute("SELECT * FROM spaces WHERE id = %s",[id])
         return Space(rows[0]['id'], rows[0]['description'], rows[0]['name'], rows[0]['bedrooms'], rows[0]['price'], rows[0]['country'], rows[0]['city'], rows[0]['booked_dates'], rows[0]['owner_id'])
-    
+
     def create(self, space):
-        self.db_connection.execute("INSERT INTO spaces (description, name, bedrooms, price, country, city, booked_dates, owner_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", [space.description, space.name, space.bedrooms, space.price, space.country, space.city, space.booked_dates, space.owner_id])
-    
+        #checks if space with same name, country, city already exists, does not create new one if it does
+        if not [space.name, space.country, space.city] in [[i.name, i.country, i.city] for i in self.get_all()]:
+            return self.db_connection.execute('INSERT INTO spaces (description, name, bedrooms, price, country, city, booked_dates, owner_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) RETURNING name', [space.description, space.name, space.bedrooms, space.price, space.country, space.city, space.booked_dates, space.owner_id])[0]
+
     def delete(self, id):
         self.db_connection.execute("DELETE FROM spaces WHERE id = %s", [id])
-    
+
     def update(self, id, space):
         self.db_connection.execute("UPDATE spaces SET description = %s, name = %s, bedrooms = %s, price = %s, country = %s, city = %s, booked_dates = %s WHERE id = %s", [space.description, space.name, space.bedrooms, space.price, space.country, space.city, space.booked_dates, id])
--- routes/spaces_routes.py
+++ routes/spaces_routes.py
@@ -5,6 +5,7 @@ from lib.space_repository import SpaceRepository
 from lib.space import Space
 from lib.bookings_repository import BookingsRepository
 from lib.bookings import Bookings
+from lib.forms import NewSpaceForm
 from datetime import datetime
 
 def spaces_routes(app):
@@ -17,3 +18,21 @@ def spaces_routes(app):
     def view_my_spaces():
         spaces = SpaceRepository(get_flask_database_connection(app)).get_all()
         return render_template('view-my-spaces.html', spaces=spaces)
+
+    @app.route('/new-space', methods=['GET', 'POST'])
+    def new_space():
+        form = NewSpaceForm()
+
+        if request.method == 'POST':
+            if form.validate_on_submit():
+                space = Space(None, form.description.data, form.name.data, form.bedrooms.data, form.price.data, form.country.data, form.city.data, [], session['email'])
+                space_return = SpaceRepository(get_flask_database_connection(app)).create(space)
+                print('####################')
+                print(space_return)
+                if space_return:
+                    flash(f'You have created new space named {space_return['name']}')
+                    return redirect('/view-my-spaces')
+                else:
+                    flash('Space with such name and location already exists')
+                    return render_template('new-space.html', form=form)
+        return render_template('new-space.html', form=form)
